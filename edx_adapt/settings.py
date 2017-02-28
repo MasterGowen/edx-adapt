4837151d3297893bf393ff063934617b042ed650
Alexander Zayats <z4y4ts@users.noreply.github.com>
2017-02-28 17:12:43 +0200

    Add TravisCI support

--- edx_adapt/settings.py
+++ edx_adapt/settings.py
@@ -1,1 +1,3 @@
-LOGS_DIR = '/var/log/edx-adapt/'
+# FIXME(idegtiarov) Log dir is set to the project dir to avoid changing dirs permissions in travis tests runs. Should be
+# changed to the appropriate log dir on production.
+LOGS_DIR = 'log/edx-adapt/'
